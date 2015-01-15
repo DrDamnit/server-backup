cf97ab1fc060f4b8483bd0872deba3e6e19e12b7
root <root@mail.relationshipheadquarters.com>
2015-01-15 12:19:11 -0500

    First working version.

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 #Backup Your Linux Box!
-This script will backup a given user's directories on a server, and will detecte Wordpress installations as well. It will dump the corresponding databases, and send them to a remote server of your choice!
+This script will backup a given user's directories on a server, and will detect Wordpress installations as well. It will dump the corresponding databases, and send them to a remote server of your choice!
+
+# How it works 
+1. It scans the home folder for user's usernames.
+2. It compares those names with the contents of /etc/passwd. If it finds a match, it keeps those usernames. NOTE: It is assuming that you have created users with the useradd -m [username] command, which creates directories that are the same name as the username.
+3. It recurses through those directories looking for a wp-config.php file. If it finds one, it parses that file for the following values:
+  * DB_NAME
+  * DB_PASSWORD
+  *DB _USER
+  *DB_HOST
+
+  If it finds those values, it uses them to dump the wordpress database to a sql file that is stored in the temporary directories for each of those users.
+4. It uses rsync to copy all the directories to a temporary location (/root/tmp/)
+5. Once all users have been backed up, it will dump the entire MySQL database (snapshot) to that same temp directory. Of course, you'll need to define the value of  mysqlrootpass= at the top of the script, or this will fail and continue on. It's not magic. It needs the password.
+6. Lastly, it will use rsync over SSH to securely copy all that data elsewhere.
+
+A note on cleanup: the final line of the script may be uncommented if you want it to delete all these temp files after the operation completes; however, if you leave it commented, the script will  clean up the /root/tmp directory before creating a backup, so old data is deleted immediately before a fresh backup is created.
+
+#Prequisites
+
+This script assumes:
+1. You are using ssh with key authentication to transfer files. This means:
+  1. You have generated ssh keys, and have exchanged the PUBLIC keys between the two servers.
+  2. You're OK with using rsync via ssh to move files from one place to another.
+2. You have some knowledge of Python. Or some programming language, and can configure your own variables.
+
 
 #Version History
 Current Status: Broken. In development. Don't use.
--- backup-server.py
+++ backup-server.py
@@ -3,6 +3,14 @@ import os
 import logging
 import re
 import getpass
+import time
+import datetime
+import shutil
+
+mysqlrootpass=[CHANGETHIS]
+sitename=[CHANGETHIS]
+servername=[CHANGETHIS]
+remoteserver=[CHANGETHIS]
 
 if not getpass.getuser() == 'root':
         sys.exit("You must run this script as root. Quitting.")
@@ -15,6 +23,27 @@ def logit(message):
     logging.info(message)
     return
 
+def dumpDB(Credentials,user):
+    filename = time.strftime("%Y-%m-%d_%H-%M-%S.sql",time.gmtime())
+    backupdir = "/home/%s/%s/" % (user,'backup')
+    backupfile = os.path.join(backupdir,filename)
+    if not os.path.exists(backupdir):
+        logit("Backup directory %s does not exist. Creating it." % backupdir)
+        os.makedirs(backupdir)
+        if os.path.exists(backupdir):
+            logit("Backup directory %s was created, and exists... changing ownership to: %s:%s" % (backupdir,user,user))
+            cmd = "chown -R %s:%s %s" % (user,user,backupdir)
+            os.system(cmd)
+    #Clean out the previous backups.
+    for f in os.listdir(backupdir):
+        if f.endswith(".sql"):
+            logit("Removing old sql backup: %s" % os.path.join(backupdir,f))
+            os.remove(os.path.join(backupdir,f))
+
+    cmd = "mysqldump --databases %s -u%s -p%s > %s" % (Credentials['DB_NAME'], Credentials['DB_USER'], Credentials['DB_PASSWORD'],backupfile)
+    logit("Dumping with command: %s" % cmd)
+    os.system(cmd)
+
 def discoverWP(user):
     root = "/home/"+user
     for root,dirs,files in os.walk(root):
@@ -22,11 +51,20 @@ def discoverWP(user):
             if f=="wp-config.php":
                 configPath = os.path.join(root,f)
                 logit("Wordpress Config Found: %s" % configPath)
-                getWPSetting(configPath)
+                Credentials = getWPSetting(configPath)
+                logit("------------------------------")
+                logit("User: %s" % Credentials['DB_USER'])
+                logit("Pass: %s" % Credentials['DB_PASSWORD'])
+                logit("Server: %s" % Credentials['DB_HOST'])
+                logit("Database: %s" % Credentials['DB_NAME'])
+                logit("------------------------------")
+                dumpDB(Credentials,user)
+
+
 
 #Returns a dictionary of key value paris with the Wordpress database credentials and information
 def getWPSetting(path):
-    creds = {}
+    creds = []
     if not os.path.exists(path):
         logging.warning("Could not parse % the file does not exist?" % path)
         return False
@@ -49,13 +87,27 @@ def getWPSetting(path):
                 logit("Parsing for: %s" % v)
                 result = re.search(pattern,line)
                 if not result == None:
-                    print "Parsed Result: %s" % result.group(1)
-                    kv = {v:result.group(1)}
-                    creds.append(kv)
+                    logit("Parsed Result: %s" % result.group(1))
+                    creds.append((v,result.group(1)))
+    Credentials=dict(creds)
+    return Credentials
 
 
 
 root = "/home/"
+backupdir = '/root/tmp/'
+if not os.path.exists(backupdir):
+    os.makedirs(backupdir)
+else:
+    #Clean up old files
+    for f in os.listdir(backupdir):
+        x = os.path.join(backupdir,f)
+        print "Removing: %s" % x
+        os.remove(x)
+
+#Set Backup Timesta
+ts = time.strftime("%Y-%m-%d_%H-%M-%S",time.gmtime())
+
 users=[]
 for d in os.listdir(root):
         path = os.path.join(root,d)
@@ -80,5 +132,25 @@ for u in users:
         logit("%s was not in /etc/passwd. We're not backing that up." %u)
         users.remove(u)
 
+#Dump the Wordpress Databases
 for u in users:
     discoverWP(u)
+
+    #Tar up all the user profiles to a specified location.
+    cmd = "tar -zcvf /root/tmp/%s-%s.tar.gz /home/%s " % (u,ts,u)
+    #cmd = "tar -zcvf /home/%s /root/tmp/%s-%s.tar.gz" % (u,u,ts)
+    print "Executing: %s" % cmd
+    os.system(cmd)
+
+
+#Do a full database dump to the same location
+cmd = "mysqldump --all-databases -uroot -p%s > /root/tmp/fulldump-%s.sql" % (mysqlrootpass,ts)
+os.system(cmd)
+
+#Transfer offsite for safe keeping
+cmd = "rsync -Pravdtze ssh /root/tmp/* %s:/mnt/raid/backups/%s/%s-%s" % (remoteserver,sitename,servername,ts)
+print "Running command: %s" % cmd
+os.system(cmd)
+
+#cleanup
+#shutil.rmtree('/root/tmp/')
